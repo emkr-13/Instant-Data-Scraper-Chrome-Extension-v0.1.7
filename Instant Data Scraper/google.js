229426265c45bce7f38a5b732dfe46940e80756b
emkr-13 <krisna.rata13@gmail.com>
2024-10-29 11:25:22 +0700

    merapikan

--- Instant Data Scraper/google.js
+++ Instant Data Scraper/google.js
@@ -6,13 +6,15 @@
   ga('create', 'UA-45974197-11', 'auto');
   ga('send', 'pageview');*/
 
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = 'https://ssl.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+(function () {
+  var ga = document.createElement("script");
+  ga.type = "text/javascript";
+  ga.async = true;
+  ga.src = "https://ssl.google-analytics.com/ga.js";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(ga, s);
 })();
 
-
 var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-45974197-11']);
-_gaq.push(['_trackPageview']);
+_gaq.push(["_setAccount", "UA-45974197-11"]);
+_gaq.push(["_trackPageview"]);
